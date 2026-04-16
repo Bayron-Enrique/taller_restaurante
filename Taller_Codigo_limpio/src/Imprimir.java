51cdbc9a6f243d46ca3fcdd5adafcec6059e3cb8
Bayron Enrique Lobo Lopez <belobo@unicesar.edu.co>
2026-04-15 23:12:49 -0500

    refactor: dividir logica de facturacion en metodos pequeños y eliminar numeros magicos en Proceso

--- Taller_Codigo_limpio/src/Imprimir.java
+++ Taller_Codigo_limpio/src/Imprimir.java
@@ -1,4 +1,4 @@
-package com.mycompany.restauranteelbuensabor;
+
 
 public class Imprimir {
 
--- Taller_Codigo_limpio/src/Proceso.java
+++ Taller_Codigo_limpio/src/Proceso.java
@@ -1,64 +1,68 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
-/**
- *
- * @author alfre
- */
 public class Proceso {
-public static double hacerTodo(){
-double sub=0;double iva=0;double tot=0;double aux=0;int cont=0;int i=0;
-while(i<Datos.nom.length){
-if(Datos.cant[i]>0){
-// multiplica precio por cantidad
-sub=sub+Datos.p[i]*Datos.cant[i];
-cont=cont+1;}
-i++;}// fin while
-if(cont>3){
-if(sub>0){
-aux=sub-(sub*0.05);
-if(aux>50000){
-iva=aux*0.19;
-// suma iva al subtotal con descuento
-tot=aux+iva;
-tot=tot+(tot*0.10);}
-else{
-// suma iva al subtotal
-iva=aux*0.19;
-tot=aux+iva;}}// fin if sub>0
-// version anterior - no borrar
-// sub = sub * 1.19;
-// if(sub > 40000) sub = sub + (sub*0.10);
-// return sub;
-}else{
-if(sub>50000){
-iva=sub*0.19;
-// suma iva al subtotal
-tot=sub+iva;
-tot=tot+(tot*0.10);}
-else{
-iva=sub*0.19;
-tot=sub+iva;}}// fin if-else cont
-Datos.est=1;Datos.tot=tot;
-return tot;}
-public static double procesar(double a,double b,double c,double d,double e,int f,boolean g){
-double res=0;double iva=0;double prop=0;double tmp=0;
-// calcula subtotal con cantidad
-res=a*b;
-if(c>0){
-// aplica descuento
-res=res-(res*c);}
-// calcula iva
-iva=res*d;tmp=iva;
-res=res+tmp;
-if(g){
-// aplica propina si corresponde
-prop=res*e;
-res=res+prop;}
-if(f>3){
-res=res-(res*0.01);}
-return res;}
+
+    // Constantes (eliminan magic numbers)
+    private static final double TASA_IVA = 0.19;
+    private static final double TASA_PROPINA = 0.10;
+    private static final double TASA_DESCUENTO = 0.05;
+    private static final double UMBRAL_PROPINA = 50000;
+    private static final int MIN_ITEMS_DESCUENTO = 3;
+
+    public static double calcularTotalFactura() {
+        double subtotal = calcularSubtotal();
+        int cantidadItems = contarItems();
+
+        double subtotalConDescuento = aplicarDescuento(subtotal, cantidadItems);
+        double iva = calcularIVA(subtotalConDescuento);
+        double propina = calcularPropina(subtotalConDescuento + iva);
+
+        double total = subtotalConDescuento + iva + propina;
+
+        // Actualizar estado (se mantiene comportamiento original)
+        Datos.est = 1;
+        Datos.tot = total;
+
+        return total;
+    }
+
+    private static double calcularSubtotal() {
+        double subtotal = 0;
+
+        for (int i = 0; i < Datos.nom.length; i++) {
+            if (Datos.cant[i] > 0) {
+                subtotal += Datos.p[i] * Datos.cant[i];
+            }
+        }
+
+        return subtotal;
+    }
+
+    private static int contarItems() {
+        int contador = 0;
+
+        for (int i = 0; i < Datos.cant.length; i++) {
+            if (Datos.cant[i] > 0) {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+
+    private static double aplicarDescuento(double subtotal, int cantidadItems) {
+        if (cantidadItems > MIN_ITEMS_DESCUENTO) {
+            return subtotal - (subtotal * TASA_DESCUENTO);
+        }
+        return subtotal;
+    }
+
+    private static double calcularIVA(double base) {
+        return base * TASA_IVA;
+    }
+
+    private static double calcularPropina(double totalConIva) {
+        if (totalConIva > UMBRAL_PROPINA) {
+            return totalConIva * TASA_PROPINA;
+        }
+        return 0;
+    }
 }
